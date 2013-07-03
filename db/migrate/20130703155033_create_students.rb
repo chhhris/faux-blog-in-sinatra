9f20bb22e28d5a70955a9d6feb5b57f97e730514
chhhris <mr.lake@gmail.com>
2013-07-03 11:59:40 -0400

    added db

--- db/migrate/20130703155033_create_students.rb
+++ db/migrate/20130703155033_create_students.rb
@@ -3,9 +3,11 @@ class CreateStudents < ActiveRecord::Migration
     create_table :students do |t|
       t.string :name
       t.string :url
+    end
   end
 
   def down
     drop_table :students
   end
+
 end
