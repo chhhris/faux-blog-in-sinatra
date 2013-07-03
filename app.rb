674258fffde045abc9b9826637aefd4db83d5469
chhhris <mr.lake@gmail.com>
2013-07-03 12:21:48 -0400

    populate database

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
-require "sinatra"
-require "sinatra/activerecord"
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+Dir.glob('./lib/*.rb') do |model|
+  require model
+end
 
 set :database, "sqlite3:///student.db"
+
+module Blog
+  class App < Sinatra::Application
+
+  end
+
+end
---
+++ config.ru
@@ -0,0 +1,5 @@
+# config.ru
+# require File.join(File.dirname(__FILE__), 'app.rb')
+
+require "./app"
+run Blog::App
